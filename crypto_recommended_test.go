19be075f98f63f1423fb3a86730c2150400cd963
Ivan Tomilov <tiabc.work@gmail.com>
2017-06-12 16:32:34 +0700

    Wrote TestDefaultCrypto_DH

--- crypto_recommended_test.go
+++ crypto_recommended_test.go
@@ -11,14 +11,32 @@ func TestDefaultCrypto_GenerateDH(t *testing.T) {
 
 	// Act.
 	pair, err := c.GenerateDH()
-
-	// Assert.
 	require.Nil(t, err)
 
-	require.Equal(t, byte(0), pair.PrivateKey[0]&7)
-	require.Equal(t, byte(0), pair.PrivateKey[31]&128)
-	require.Equal(t, byte(64), pair.PrivateKey[31]&64)
+	// Assert.
+	require.EqualValues(t, 0, pair.PrivateKey[0]&7)
+	require.EqualValues(t, 0, pair.PrivateKey[31]&128)
+	require.EqualValues(t, 64, pair.PrivateKey[31]&64)
 
 	require.Len(t, pair.PrivateKey, 32)
 	require.Len(t, pair.PublicKey, 32)
+}
+
+func TestDefaultCrypto_DH(t *testing.T) {
+	// Arrange.
+	c := DefaultCrypto{}
+
+	// Act.
+	var (
+		alicePair, err1 = c.GenerateDH()
+		bobPair, err2   = c.GenerateDH()
+		aliceSK         = c.DH(alicePair, bobPair.PublicKey)
+		bobSK           = c.DH(bobPair, alicePair.PublicKey)
+	)
+
+	// Assert.
+	require.Nil(t, err1)
+	require.Nil(t, err2)
+	require.NotEqual(t, [32]byte{}, aliceSK)
+	require.Equal(t, aliceSK, bobSK)
 }
