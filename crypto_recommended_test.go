88a23a9c848440831417a7393f14f7ec7cdb71ce
Ivan Tomilov <tiabc.work@gmail.com>
2017-06-12 16:39:39 +0700

    Wrote TestDefaultCrypto_GenerateDH_DifferentKeysEveryTime

--- crypto_recommended_test.go
+++ crypto_recommended_test.go
@@ -5,21 +5,46 @@ import (
 	"testing"
 )
 
-func TestDefaultCrypto_GenerateDH(t *testing.T) {
+func TestDefaultCrypto_GenerateDH_Basic(t *testing.T) {
 	// Arrange.
 	c := DefaultCrypto{}
 
 	// Act.
 	pair, err := c.GenerateDH()
-	require.Nil(t, err)
 
 	// Assert.
+	require.Nil(t, err)
+
 	require.EqualValues(t, 0, pair.PrivateKey[0]&7)
 	require.EqualValues(t, 0, pair.PrivateKey[31]&128)
 	require.EqualValues(t, 64, pair.PrivateKey[31]&64)
 
 	require.Len(t, pair.PrivateKey, 32)
 	require.Len(t, pair.PublicKey, 32)
+}
+
+func TestDefaultCrypto_GenerateDH_DifferentKeysEveryTime(t *testing.T) {
+	// Arrange.
+	var (
+		c    = DefaultCrypto{}
+		keys = make(map[[32]byte]bool)
+	)
+
+	for i := 0; i < 100; i++ {
+		t.Run("", func(t *testing.T) {
+			// Act.
+			pair, err := c.GenerateDH()
+
+			// Assert.
+			require.Nil(t, err)
+			require.False(t, keys[pair.PrivateKey])
+			require.False(t, keys[pair.PublicKey])
+
+			// Preserve.
+			keys[pair.PrivateKey] = true
+			keys[pair.PublicKey] = true
+		})
+	}
 }
 
 func TestDefaultCrypto_DH(t *testing.T) {
