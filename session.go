47d1cda18ce0b0a4b08804d8f8bc609aa2db7559
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-08-21 11:12:32 +0200

    Allow to store and load sessions (#3)
    * Allow storing and loading sessions

--- session.go
+++ session.go
@@ -6,28 +6,33 @@ import "fmt"
 type Session interface {
 	// RatchetEncrypt performs a symmetric-key ratchet step, then AEAD-encrypts the message with
 	// the resulting message key.
-	RatchetEncrypt(plaintext, associatedData []byte) Message
+	RatchetEncrypt(plaintext, associatedData []byte) (Message, error)
 
 	// RatchetDecrypt is called to AEAD-decrypt messages.
 	RatchetDecrypt(m Message, associatedData []byte) ([]byte, error)
 }
 
-type session struct {
-	state
+type sessionState struct {
+	id []byte
+	State
+	storage SessionStorage
 }
 
 // New creates session with the shared key.
-func New(sharedKey Key, keyPair DHPair, opts ...option) (Session, error) {
+func New(id []byte, sharedKey Key, keyPair DHPair, storage SessionStorage, opts ...option) (Session, error) {
 	state, err := newState(sharedKey, opts...)
 	if err != nil {
 		return nil, err
 	}
 	state.DHs = keyPair
-	return &session{state}, nil
+
+	session := &sessionState{id: id, State: state, storage: storage}
+
+	return session, session.store()
 }
 
 // NewWithRemoteKey creates session with the shared key and public key of the other party.
-func NewWithRemoteKey(sharedKey, remoteKey Key, opts ...option) (Session, error) {
+func NewWithRemoteKey(id []byte, sharedKey, remoteKey Key, storage SessionStorage, opts ...option) (Session, error) {
 	state, err := newState(sharedKey, opts...)
 	if err != nil {
 		return nil, err
@@ -38,12 +43,46 @@ func NewWithRemoteKey(sharedKey, remoteKey Key, opts ...option) (Session, error)
 	}
 	state.DHr = remoteKey
 	state.SendCh, _ = state.RootCh.step(state.Crypto.DH(state.DHs, state.DHr))
-	return &session{state}, nil
+
+	session := &sessionState{id: id, State: state, storage: storage}
+
+	return session, session.store()
+}
+
+// Load a session from a SessionStorage implementation and apply options.
+func Load(id []byte, store SessionStorage, opts ...option) (Session, error) {
+	state, err := store.Load(id)
+	if err != nil {
+		return nil, err
+	}
+
+	if state == nil {
+		return nil, nil
+	}
+
+	if err = state.applyOptions(opts); err != nil {
+		return nil, err
+	}
+
+	s := &sessionState{id: id, State: *state}
+	s.storage = store
+
+	return s, nil
+}
+
+func (s *sessionState) store() error {
+	if s.storage != nil {
+		err := s.storage.Save(s.id, &s.State)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // RatchetEncrypt performs a symmetric-key ratchet step, then encrypts the message with
 // the resulting message key.
-func (s *session) RatchetEncrypt(plaintext, ad []byte) Message {
+func (s *sessionState) RatchetEncrypt(plaintext, ad []byte) (Message, error) {
 	var (
 		h = MessageHeader{
 			DH: s.DHs.PublicKey(),
@@ -53,11 +92,17 @@ func (s *session) RatchetEncrypt(plaintext, ad []byte) Message {
 		mk = s.SendCh.step()
 	)
 	ct := s.Crypto.Encrypt(mk, plaintext, append(ad, h.Encode()...))
-	return Message{h, ct}
+
+	// Store state
+	if err := s.store(); err != nil {
+		return Message{}, err
+	}
+
+	return Message{h, ct}, nil
 }
 
 // RatchetDecrypt is called to decrypt messages.
-func (s *session) RatchetDecrypt(m Message, ad []byte) ([]byte, error) {
+func (s *sessionState) RatchetDecrypt(m Message, ad []byte) ([]byte, error) {
 	// Is the message one of the skipped?
 	mk, ok, err := s.MkSkipped.Get(m.Header.DH, uint(m.Header.N))
 	if err != nil {
@@ -75,7 +120,7 @@ func (s *session) RatchetDecrypt(m Message, ad []byte) ([]byte, error) {
 
 	var (
 		// All changes must be applied on a different session object, so that this session won't be modified nor left in a dirty session.
-		sc state = s.state
+		sc = s.State
 
 		skippedKeys1 []skippedKey
 		skippedKeys2 []skippedKey
@@ -104,9 +149,20 @@ func (s *session) RatchetDecrypt(m Message, ad []byte) ([]byte, error) {
 	}
 
 	// Apply changes.
-	s.applyChanges(sc, append(skippedKeys1, skippedKeys2...))
+	if err := s.applyChanges(sc, append(skippedKeys1, skippedKeys2...)); err != nil {
+		return nil, err
+	}
+
 	if isDHStepped {
-		s.deleteSkippedKeys(s.DHr)
+		err = s.deleteSkippedKeys(s.DHr)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Store state
+	if err := s.store(); err != nil {
+		return nil, err
 	}
 
 	return plaintext, nil
